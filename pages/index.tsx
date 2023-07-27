c6070098edecca54839715dd2fbc20b9e9bce700
Ashton Esparza <ashton.esparza@us.af.mil>
2023-07-27 00:07:46 -0400

    invalidates cached query to trigger re-run

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { useState } from "react";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 type Todo = {
   id: string;
@@ -45,6 +45,8 @@ const createTodo = async (newTodoTitle: String) => {
 export default function Home() {
   const [newTodoTitle, setNewTodoTitle] = useState("");
 
+  const queryClient = useQueryClient();
+
   const { isLoading, isError, data, error } = useQuery({
     queryKey: ["todos"],
     queryFn: getTodos,
@@ -53,6 +55,10 @@ export default function Home() {
   const mutation = useMutation({
     mutationFn: (newTodo: string) => {
       return createTodo(newTodo);
+    },
+    onSuccess: () => {
+      console.log("mutation was successful....");
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
     },
   });
 
@@ -78,16 +84,20 @@ export default function Home() {
             }}
           ></input>
           <button>Create Todo</button>
+          {mutation.isLoading && <p>Creating todo...</p>}
+          {mutation.isSuccess && <p>Todo Created...</p>}
         </form>
       </div>
 
-      {isLoading ? (
-        <p>Loading todos...</p>
-      ) : isError ? (
-        <p>Error...</p>
-      ) : (
-        data.todos.map((todo) => <p key={todo.id}>{todo.title}</p>)
-      )}
+      <div>
+        {isLoading ? (
+          <p>Loading todos...</p>
+        ) : isError ? (
+          <p>Error...</p>
+        ) : (
+          data.todos.map((todo) => <p key={todo.id}>{todo.title}</p>)
+        )}
+      </div>
     </>
   );
 }
@@ -98,3 +108,6 @@ export default function Home() {
 // 3: Create JSX to display todos
 // 4: Create mutation
 // 5: Call mutation
+// 6: Show JSX examples and callback from mutation state
+// 7: Create query client with useQueryClient
+// 8: Invalidate todos query
