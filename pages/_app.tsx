ef4630033cdabfc747b0b123d5c2c067cf3be336
Ashton Esparza <ashton.esparza@us.af.mil>
2023-07-27 12:08:44 -0400

    demo app is now designed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <main className={inter.className}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </main>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,23 @@
 import Head from "next/head";
 import { useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import styles from "../styles/home.module.css";
+
+// TanStack Query Demo
+//
+// Goals:
+// List a collection of exisitng Todos through a query
+// Create Todos using a mutation
+//
+// Steps:
+// 1: Wrap app with QueryClientProvider
+// 2: Call useQuery
+// 3: Create JSX to display todos
+// 4: Create mutation
+// 5: Call mutation
+// 6: Show JSX examples and callback from mutation state
+// 7: Create query client with useQueryClient
+// 8: Invalidate todos query
 
 type Todo = {
   id: string;
@@ -68,46 +85,41 @@ export default function Home() {
         <title>TanStack Query Demo</title>
       </Head>
 
-      <div>
-        <form
-          onSubmit={async (e) => {
-            e.preventDefault();
-            mutation.mutate(newTodoTitle);
-            setNewTodoTitle("");
-          }}
-        >
-          <label>New Todo</label>
-          <input
-            value={newTodoTitle}
-            onChange={(e) => {
-              setNewTodoTitle(e.target.value);
+      <div className={styles.todoContainer}>
+        <div>
+          <form
+            className={styles.todoForm}
+            onSubmit={async (e) => {
+              e.preventDefault();
+              // Mutation here
+              mutation.mutate(newTodoTitle);
+              setNewTodoTitle("");
             }}
-          ></input>
-          <button>Create Todo</button>
+          >
+            <label>New Todo</label>
+            <input
+              value={newTodoTitle}
+              onChange={(e) => {
+                setNewTodoTitle(e.target.value);
+              }}
+            ></input>
+            <button>Create Todo</button>
+          </form>
           {mutation.isLoading && <p>Creating todo...</p>}
           {mutation.isSuccess && <p>Todo Created...</p>}
-        </form>
-      </div>
+        </div>
 
-      <div>
-        {isLoading ? (
-          <p>Loading todos...</p>
-        ) : isError ? (
-          <p>Error...</p>
-        ) : (
-          data.todos.map((todo) => <p key={todo.id}>{todo.title}</p>)
-        )}
+        <div className={styles.todoList}>
+          {/* Todos Here */}
+          {isLoading ? (
+            <p>Loading todos...</p>
+          ) : isError ? (
+            <p>Error...</p>
+          ) : (
+            data.todos.map((todo) => <p key={todo.id}>{todo.title}</p>)
+          )}
+        </div>
       </div>
     </>
   );
 }
-
-// Steps
-// 1: Wrap app with QueryClientProvider
-// 2: Wire up useQuery
-// 3: Create JSX to display todos
-// 4: Create mutation
-// 5: Call mutation
-// 6: Show JSX examples and callback from mutation state
-// 7: Create query client with useQueryClient
-// 8: Invalidate todos query
