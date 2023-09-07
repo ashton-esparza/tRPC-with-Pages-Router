4b7b411e40b974260e519cb63925b7fc4ba511b5
Ashton Esparza <ashton.esparza@us.af.mil>
2023-09-07 11:50:14 -0400

    setup tRPC vanilla codebase structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,23 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Inter } from "next/font/google";
+import { trpc } from "../utils/trpc";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
+    // <main className={inter.className}>
+    //   <QueryClientProvider client={queryClient}>
+    //     <Component {...pageProps} />
+    //   </QueryClientProvider>
+    // </main>
     <main className={inter.className}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
+      <Component {...pageProps} />
     </main>
   );
 }
+
+export default trpc.withTRPC(App);
